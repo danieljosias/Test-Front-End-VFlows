eb91865ade9d68184fb9406528cf15a5f87bb523
Daniel Josias <daniieljosiaslima@gmail.com>
2023-01-28 10:56:00 -0300

    feat: contexto do usuário e dos contratos

---
+++ src/contexts/Contracts/contractContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+import { contracts } from "../../mocks/contracts";
+
+export const ContractContext = createContext();
+
+export const ContractContextProvider = ({ children }) => {
+    const [contractData, setContractData] = useState(contracts);
+
+    return (
+        <ContractContext.Provider
+          value={{contractData}}
+        >
+          {children}
+        </ContractContext.Provider>
+    )
+}
