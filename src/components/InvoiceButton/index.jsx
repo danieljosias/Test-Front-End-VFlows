20e3258050a8d7341fceaa67fa5a4713ab7159ae
Daniel Josias <daniieljosiaslima@gmail.com>
2023-01-28 17:38:57 -0300

    feat: botão do componente da nota fiscal

---
+++ src/components/InvoiceButton/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Wrapper } from './styles'
+import Button from '../Button'
+
+export default function InvoiceButton() {
+  return (
+    <Container>
+      <Wrapper>
+        <Button width='250px' padding='5px' background='var(--yellow)' color='var(--white)' weight='bold' size='18px' content='Anterior' />
+        <Button width='250px' padding='5px' background='var(--green)' color='var(--white)' weight='bold' size='18px' content='Próximo' />
+      </Wrapper>
+    </Container>
+  )
+}
