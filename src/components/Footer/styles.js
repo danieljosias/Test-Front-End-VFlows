cef9c32cff94ef7cfe29eb06128aa8bc90459811
Daniel Josias <daniieljosiaslima@gmail.com>
2023-01-27 22:16:07 -0300

    feat: componente footer

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+export const Footer = styled.footer`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 1000px;
+    position: absolute;
+    bottom: 0;
+
+    @media (max-width: 768px){
+        width: 720px;
+    }
+`;
+
+export const Logo = styled.img`
+    width: 150px;
+`;
+
+export const Text = styled.p`
+    color: var(--grey-1);
+`;
